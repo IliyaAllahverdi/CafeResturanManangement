f3ba7af3f52c8262f7b78192e403c69305d8e1c7
Iliya <acmilankakam@gmail.com>
2026-07-21 00:56:06 -0700

    init

---
+++ CafeResturanManangement/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CafeResturanManangement;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
